b3cba373a46c87f2dd14e8b85098f6698c5f5776
Patty <63872653+patty2904@users.noreply.github.com>
2021-04-22 22:12:32 +0100

    created board

--- connectfour.c
+++ connectfour.c
@@ -1,16 +1,45 @@
 /* Patrycja Chrzaszcz
-Last modified: 19/04/2021
+Last modified: 22/04/2021
 Connect four game in C */
 
 #include <stdio.h>
+#include<stdbool.h>  
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
-int main(int argc, char **argv)
-{
+int main(void) {
 
-return 0;
+char boardd[6][7] = {
+    {'-','-', '-', '-', '-', '-', '-', },
+    {'-','-', '-', '-', '-', '-', '-', },
+    {'-','-', '-', '-', '-', '-', '-', },
+    {'-','-', '-', '-', '-', '-', '-', },
+    {'-','-', '-', '-', '-', '-', '-', },
+    {'-','-', '-', '-', '-', '-', '-', }
+};
+
+char board[6][7];
+
+for (int i = 0; i < 6; i ++){
+  for (int j = 0; j < 7; j++){
+    board[i][j] = '-';
+  }
+}
+
+int alph = 17;
+printf("%s", "  1 2 3 4 5 6 7\n");
+for (int i = 0; i < 6; i ++){
+    printf("%c ", alph + '0'); 
+    alph++;
+  for (int j = 0; j < 7; j++){
+    printf("%c%s", board[i][j], " ");
+    if(j==6){
+    printf("\n");
+    }
+    }
+  }
+
+
+  return 0;
 }
 
 /* SOURCES

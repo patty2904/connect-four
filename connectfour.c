13dfb1c5b75a94a200686afcf1183a05e7a8cfe4
Patty <63872653+patty2904@users.noreply.github.com>
2021-04-28 23:48:56 +0100

    horizontal and vertical win check

--- connectfour.c
+++ connectfour.c
@@ -9,6 +9,156 @@ Connect four game in C */
 
 char board[6][7];
 
+void checkHorizontal(verticalBoard, horizontalBoard, character)
+{
+  //FOR MIDDLE PIECES
+  //vertical must stay the same
+  //check that horizontal + 1 = player token
+  //AND horizontal - 1 = player token
+  //so long as horizontal not <= 0 or not => 6
+  //then do check that horizontal + 2 = player token
+  //OR horizontal - 2 = player token
+  if (horizontalBoard > 0 && horizontalBoard < 6)
+  {
+    if(
+    (board[verticalBoard][horizontalBoard + 1] == character) &&
+    (board[verticalBoard][horizontalBoard - 1] == character) )
+      {
+      if ( 
+        (board[verticalBoard][horizontalBoard + 2] == character) ||
+        (board[verticalBoard][horizontalBoard - 2] == character) )
+        {
+      printf("Player %c has won!\n", character);
+      exit(0);
+        }
+      }
+  
+
+  else if(
+    (board[verticalBoard][horizontalBoard - 1] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard - 2] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard - 3] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+
+    else if(
+    (board[verticalBoard][horizontalBoard + 1] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard + 2] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard + 3] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+  }
+  else if(horizontalBoard == 0) {
+    if(
+    (board[verticalBoard][horizontalBoard + 1] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard + 2] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard + 3] == character) )
+    {
+    printf("Player %c has won!\n", character);
+    exit(0);
+    }
+  }
+  
+
+  else if(horizontalBoard == 6) {
+     if(
+    (board[verticalBoard][horizontalBoard - 1] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard - 2] == character) 
+    &&
+    (board[verticalBoard][horizontalBoard - 3] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+  } 
+}
+
+
+
+void checkVertical(verticalBoard, horizontalBoard, character)
+{
+if (verticalBoard > 0 && verticalBoard < 5)
+  {
+    if(
+    (board[verticalBoard + 1][horizontalBoard] == character) &&
+    (board[verticalBoard - 1][horizontalBoard] == character) )
+      {
+      if ( 
+        (board[verticalBoard + 2][horizontalBoard] == character) ||
+        (board[verticalBoard - 2][horizontalBoard] == character) )
+        {
+      printf("Player %c has won!\n", character);
+      exit(0);
+        }
+      }
+  
+
+  else if(
+    (board[verticalBoard - 1][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard - 2][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard - 3][horizontalBoard] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+
+    else if(
+    (board[verticalBoard + 1][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard + 2][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard + 3][horizontalBoard] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+  }
+  else if(verticalBoard == 0) {
+    if(
+    (board[verticalBoard + 1][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard + 2][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard + 3][horizontalBoard] == character) )
+    {
+    printf("Player %c has won!\n", character);
+    exit(0);
+    }
+  }
+  
+
+  else if(horizontalBoard == 5) {
+     if(
+    (board[verticalBoard - 1][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard - 2][horizontalBoard] == character) 
+    &&
+    (board[verticalBoard - 3][horizontalBoard] == character) )
+    {
+    printf("Player %c has won!\n", character);
+   exit(0);
+    }
+  } 
+}
+
+void checkDiagonal (verticalBoard, horizontalBoard, player){
+  
+}
+
+
 void populateBoard(verticalBoard, horizontalBoard, player)
 {
   char character = ' ';
@@ -21,7 +171,7 @@ void populateBoard(verticalBoard, horizontalBoard, player)
   if (board[verticalBoard][horizontalBoard] == '-') {
 do{
 if(board[verticalBoard + 1][horizontalBoard] == '-'){
-      printf("You cannot place your token here.\n");
+      printf("You cannot place a token if there is space below.\n");
       exit(0);
     } else {
     board[verticalBoard][horizontalBoard] = character;
@@ -32,7 +182,7 @@ while (!((verticalBoard + 1) > 5));
 
   }
   else {
-    printf("You cannot place your token here.\n");
+    printf("Something is already in this position.\n");
   }
   
   int vertNum = 0;
@@ -96,33 +246,39 @@ while (i < 43)
   printf("Vertical position: ");
   scanf("%d", &verticalPosition);
   if (verticalPosition > 5 || verticalPosition < 0){
-    printf("You cannot place your token here.\n");
+    printf("This position does not exist.\n");
     break; 
   }
   printf("Horizontal position: ");
   scanf("%d", &horizontalPosition);
   if (horizontalPosition > 6 || horizontalPosition < 0){
-    printf("You cannot place your token here.\n");
+    printf("This position does not exist.\n");
     break; 
   }
   populateBoard(verticalPosition, horizontalPosition, firstPlayer);
+  checkHorizontal(verticalPosition, horizontalPosition, firstPlayer);
+  checkVertical(verticalPosition, horizontalPosition, firstPlayer);
   }
+
   else if (i % 2 == 0)
   {
   printf("%c, What move do you want to make?  \n", secondPlayer);
   printf("Vertical position: ");
   scanf("%d", &verticalPosition);
   if (verticalPosition > 5 || verticalPosition < 0){
-    printf("You cannot place your token here.\n");
+    printf("This position does not exist.\n");
     break; 
   }
   printf("Horizontal position: ");
   scanf("%d", &horizontalPosition);
    if (horizontalPosition > 6 || horizontalPosition < 0){
-    printf("You cannot place your token here.\n");
+    printf("This position does not exist.\n");
     break; 
   }
   populateBoard(verticalPosition, horizontalPosition, secondPlayer);
+  checkHorizontal(verticalPosition, horizontalPosition, secondPlayer);
+  checkVertical(verticalPosition, horizontalPosition, secondPlayer);
+
   }
   else {
     printf("Something went wrong...");
